35ea8c620e0eb7317dcd3a0f4b3fe3e65c7dca60
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2024-12-29 15:30:02 -0300

    Protegendo a Home com Login e UI, FontAwesome

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,11 @@
 
 use App\Middleware\AuthMiddleware;
 
-$app->get('/', 'HomeController:index')->setName('home');
+//$app->get('/', 'HomeController:index')->setName('home');
+
+$app->get('/', function($request, $response){
+  return $this->view->render($response, 'index.twig');
+})->setName('home')->add(new \App\Middleware\AuthMiddleware($container));
 
 $app->group('/auth', function($app){
   $app->map(['GET', 'POST'], '/login', 'AuthController:login')->setName('auth.login');
